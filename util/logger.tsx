d6f1aa74ca81f845f0ed83a5f5fcda827f243833
Dat Nguyen <=>
2024-12-04 14:43:00 +0700

    add

--- util/logger.tsx
+++ util/logger.tsx
@@ -6,9 +6,6 @@ const vietnamTime = () => {
   return new Intl.DateTimeFormat("vi-VN", {
     timeZone: "Asia/Ho_Chi_Minh",
     hour12: false,
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
     hour: "2-digit",
     minute: "2-digit",
     second: "2-digit",
